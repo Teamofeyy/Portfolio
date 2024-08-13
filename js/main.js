8ded6b9a73172642516ae740e48e392d336e6954
Teamofeyy <timarnd06@gmail.com>
2024-08-13 18:15:51 +0300

    add dark theme

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-const btnDarkMode = document.querySelector(".dark-mode-btn");
+document.addEventListener("DOMContentLoaded", function () {
+    const btnDarkMode = document.querySelector(".dark-mode-btn");
 
-btnDarkMode.onclick = function () {
-    btnDarkMode.classList.toggle("dark-mode-btn--active");
-}
+    btnDarkMode.onclick = function () {
+        btnDarkMode.classList.toggle("dark-mode-btn--active");
+        document.body.classList.toggle("dark")
+    }
+});
